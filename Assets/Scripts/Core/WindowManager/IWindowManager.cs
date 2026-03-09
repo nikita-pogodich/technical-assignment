a9137c66cd68a217fb94f3c9430ddcc12a862ed3
Nikita Pogodich <nikita.pogodich@gmail.com>
2026-03-09 20:46:33 +0400

    Added WindowManager

---
+++ Assets/Scripts/Core/WindowManager/IWindowManager.cs
@@ -0,0 +1,20 @@
+using Core.MVP;
+using Cysharp.Threading.Tasks;
+
+namespace Core.WindowManager
+{
+    public interface IWindowManager
+    {
+        void RegisterWindowFactory(IWindowFactory windowFactory);
+
+        UniTask ShowWindowAsync<TView, TModel>(
+            string viewName,
+            WindowShowDelegate<TModel> beforeShow = null,
+            WindowShowDelegate<TModel> alreadyShown = null)
+            where TView : IView
+            where TModel : IModel;
+
+        void HideWindow(IModel model);
+        void HideAllWindows();
+    }
+}
